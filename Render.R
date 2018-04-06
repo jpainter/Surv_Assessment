3ee5ebd69f7957722b001d39fee8b49c229d567d
Painter <bzp3@cdc.gov>
2018-04-06 13:14:54 -0400

    bug fixes

--- Render.R
+++ Render.R
@@ -1,7 +1,7 @@
 # Render doucments
 
-library(knitrBootstrap) # NB: does not work with DT::datatable
 library(rmarkdown)
+library(magrittr)
 
 # function to open an html document
     openHTML <- function(x) browseURL(paste0('file://', file.path(getwd(), x)))
@@ -10,25 +10,49 @@ library(rmarkdown)
 # This script creates the review documents 
 
     dhis_instance = "Uganda"
-    data_directory = "../HMIS/DHIS2"
+    data_directory = "../HMIS/DHIS2" # folder containing login_file 
+    output_format = 'word_document'
+    
+    # this folder will hold output of the metadata and data reviews
+    output_directory = "./" # "." is the directory where this file, Render.R, sits. 
+    if (!dir.exists( dhis_instance ) ) dir.create( dhis_instance )
     
 # list of options to pass to the markdown documents
     params.list = list(
-        origin_login_file = dhis_instance , # selects dhis2 instance
-        data_directory = data_directory ,  # directory for login and data files
-        cache = FALSE
+        dhis_instance = dhis_instance , # selects dhis2 instance
+        data_directory = data_directory ,  # root data directory
+        output_directory = output_directory ,
+        cache = FALSE ,
+        echo = FALSE 
     )
 
+    outputs = data_frame( 
+        output_format = c(  "html_document", 'word_document'),
+        output_suffix = c(  "html", 'doc')
+    )
+    
+    output.suffix = outputs %>% 
+        filter( output_format == .output_format ) %>% 
+        .$output_suffix
+    
 # Metadata
-    render('Metatdata_Review.Rmd', 
+    output.filename = paste0( output_directory, dhis_instance, "/", 
+                             dhis_instance, "_Metadata_Review." ,
+                             output.suffix
+                             ) 
+    
+    render('Metadata_Review.Rmd', 
+           output_file = output.filename ,
+           output_format = output_format ,
            # 'knitrBootstrap::bootstrap_document' ,
            params = params.list ,
            envir = new.env()
            )
-    openHTML('Metatdata_Review.html')
+    
+    openHTML( output.filename )
 
 # Select data elements to review
-    de.review.xlsx = paste0( data_directory , "/" ,
+    de.review.xlsx = paste0( output_directory , 
                              dhis_instance , "/" ,
                              dhis_instance , "_" , 
                              "dataElements.xlsx" )
@@ -37,13 +61,31 @@ library(rmarkdown)
     if ( file.exists( de.review.xlsx ) ) browseURL( de.review.xlsx )
     
 # Data
-    render('Data_Review.Rmd', 
-           # 'knitrBootstrap::bootstrap_document' ,
-           params = params.list ,
-           envir = new.env()
-           )
     
-    openHTML('Data_Review.html')
+    # if variables selected, download and analyze
+    if ( file.exists( de.review.xlsx ) ) { 
+        
+        de.include = readxl::read_xlsx( de.review.xlsx  ) %>% 
+            filter( !is.na(Include) ) %>% .$Include
+    
+        if ( length( de.include ) > 0 ){ 
+            output.filename = paste0(output_directory , dhis_instance, "/", 
+                                     dhis_instance, "_Data_Review.html" ) 
+            
+            render('Data_Review.Rmd', 
+                   output_file = output.filename ,
+                   output_format = output_format ,
+                   # 'knitrBootstrap::bootstrap_document' ,
+                   params = params.list ,
+                   envir = new.env()
+                   )
+            
+            openHTML( output.filename )
+            
+        } else {
+            cat( message("Select data elements to include"))
+        }
+    }
 
 
 # After running this script, the output will appear in the current directory
