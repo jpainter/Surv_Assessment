1217d946697c0eb1ab07e382d087f0b6d0b5b294
Painter <bzp3@cdc.gov>
2018-04-20 17:06:13 -0400

    DQ_Trend initial commit

--- dhis2_functions.R
+++ dhis2_functions.R
@@ -3,7 +3,7 @@
 # Login ####
 loginDHIS2<-function( baseurl, username, password) {
   url<-paste0( baseurl, "api/me" )
-  r <- GET(url, authenticate(username,password) )
+  r <- GET(url, authenticate(username, password) )
   assert_that( r$status_code == 200L ) }
   
 # JSON helper function ####
@@ -314,17 +314,17 @@ get = function( source_url , ...){
                       
  ){
      
-     if ( file.exists( file ) ){
-         
-         data.file = readRDS( file ) 
-         
-         # select periods not in file
-         
-         # select level not in file 
-         
-         # select vars not in file 
-         
-     }
+     # if ( file.exists( file ) ){
+     #     
+     #     data.file = readRDS( file ) 
+     #     
+     #     # select periods not in file
+     #     
+     #     # select level not in file 
+     #     
+     #     # select vars not in file 
+     #     
+     # }
      
      # Set list of elements to ask for
      dataElements = de.vars %>% .$dataElement
@@ -337,17 +337,21 @@ get = function( source_url , ...){
              # link datasets
              inner_join( dsde , by = "dataElement" 
              ) %>%
-             count( dataSet ) %>% .$dataSet %>%
+             count( dataSet ) %>% 
              # convert ids to names
              rename( id = dataSet ) %>%
              left_join( md$dataSets %>% select( name, id ), 
                         by = "id" 
              ) %>%
-             rename( dataSet =  name )
+             .$name
      }
      
      
      login_origin()
+     
+     print( baseurl )
+     # print( paste( "details:" , details ) )
+     # print( paste( "submissions:" , submissions ) )
      
      data = list()
      for ( period in seq_along( periods.vector ) ){
@@ -355,23 +359,27 @@ get = function( source_url , ...){
          print( paste( periods.vector[period] ) )
          
          data.de = list()
+         
          for ( element in  seq_along( dataElements ) ){
              
              # data elements:
              if ( details == TRUE & submissions == TRUE ) return()
-             if ( details == FALSE & submissions == FALSE ){
+             if ( submissions == FALSE ){
                  
-             de.ids = paste( 
-                 md$dataElements %>% 
-                     select( id, name ) %>%
-                     filter( trimws(name) %in% de.vars$dataElement[ element ] ) %>%
-                     .$id ,
-                 collapse  = ";" )
+                 de.ids = paste( 
+                     md$dataElements %>% 
+                         select( id, name ) %>%
+                         filter( trimws(name) %in% dataElements[ element ] ) %>%
+                         .$id ,
+                     collapse  = ";" )
              }
              
-             if (details){
+             if ( details ){
                  
                  de.index = which( md$dataElements$id %in% de.ids )
+                 
+                 # print( paste("de.index" , de.index ) )
+                 # print( paste("de.ids" , de.ids ) )
                  
                  # data.frame of dataElement-id and categorycomb0-id
                  de.catCombo = data_frame( 
@@ -393,12 +401,21 @@ get = function( source_url , ...){
                  de.ids = paste( paste0( de.catOptCombo$dataElement, "." , 
                                          de.catOptCombo$categoryOptionCombo) ,
                                  collapse  = ";" )
+                 
+                 # print( paste("data elements:" , de.ids ) )
              } 
              
              if ( submissions ){
                  
                  reports = c( 'ACTUAL_REPORTS', 'ACTUAL_REPORTS_ON_TIME', 'EXPECTED_REPORTS' )
-                 de.ids = paste0( dataElements[ element ], ".", reports , collapse = ';')
+                 # get ids
+                 de.ids = md$dataSets %>% 
+                         select( id, name ) %>%
+                         filter( name %in% dataElements[ element ] ) %>%
+                         .$id 
+                 
+                 # add report types
+                 de.ids = paste0( de.ids, ".", reports , collapse = ';')
              }
              
              print( paste( periods[ period ], "Element" , element ,
@@ -410,9 +427,9 @@ get = function( source_url , ...){
              for ( level in seq_along( levels ) ){
                  
                  # If no value for level 1, skip other levels
-                 if ( level > 1 & !is.data.frame( fetch ) ) next()
+                 if ( level > 1 && !is.data.frame( fetch ) ) next()
                  
-                 print( levels[level] )
+                 print( paste( levels[level] , ifelse( details, "Details", "") ) )
                  
                  #Assemble the URL ( before starting, double check semicolons for dx dimension )
                  url <- paste0( baseurl, "api/analytics/dataValueSet.json?" ,
@@ -426,22 +443,28 @@ get = function( source_url , ...){
                                 # "KNrK5VWTZkx;NLKRV7bYbVy" , # population
                                 "&displayProperty=NAME")
                  
+                 # print( url )
+                 
                  # Fetch data
                  fetch <- retry( get(url)[[1]] ) # if time-out or other error, will retry 
                  
                  # if returns a data frame of values (e.g. not 'server error'), then keep
                  if ( is.data.frame( fetch ) ){ 
                      
-                     data.level[[level]] = fetch %>% 
+                     data.level[[ level ]] = fetch %>% 
                          select( -storedBy, -created, -lastUpdated, -comment )
                      
-                     print( paste( nrow(fetch), "records. " ) )
+                     print( paste( nrow(fetch), "records." ) )
+                 
+                     } else {
+                         
+                     print( "no records" )
                  }
              }
              
              data.de[[ element ]] = data.table::rbindlist( data.level, fill = TRUE )
              
-             print( paste( de.vars$dataElement[ element ]  , "has" , 
+             print( paste( dataElements[ element ]  , "has" , 
                            scales::comma( nrow( data.de[[ element ]] ) ) , 
                            "records"  ) ) 
              
